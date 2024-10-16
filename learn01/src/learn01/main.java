48c3e6725dc33c6de4c72463c980a59aea571862
thuan1511 <thuannh1511@gmail.com>
2024-10-16 19:35:25 +0700

    first project

---
+++ learn01/src/learn01/main.java
@@ -0,0 +1,24 @@
+package learn01;
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        // Nhập chiều dài 2 cạnh góc vuông
+        System.out.print("Nhập chiều dài cạnh a: ");
+        double a = scanner.nextDouble();
+        
+        System.out.print("Nhập chiều dài cạnh b: ");
+        double b = scanner.nextDouble();
+
+        // Tính chiều dài cạnh huyền
+        double hypotenuse = Math.sqrt(a * a + b * b);
+
+        // Xuất kết quả
+        System.out.printf("Chiều dài cạnh huyền là: %.2f%n", hypotenuse);
+
+        scanner.close();
+	}
+
+}
